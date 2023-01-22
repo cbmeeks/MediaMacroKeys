0b7125e3aef062bd986292962cb10b239885bbf1
cbmeeks <cbmeeks@gmail.com>
2023-01-22 10:50:54 -0500

    Update code.py
    added some colored led magic!

--- code.py
+++ code.py
@@ -9,7 +9,8 @@
 #
 #	TODO:  Add MUTE button support
 #
-#	Made in 2022 by cbmeeks@gmail.com
+#	Made in 2022 by Cecil Meeks, cbmeeks@gmail.com
+#	Updates in 2023 by Cecil Meeks, cbmeeks@gmail.com
 #
 import time
 import board
@@ -28,9 +29,21 @@ KEY_MEDIA_CALC			= 0xfb
 keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard) 
 
+# onboard led of Pico
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+# colored led
+red_led = digitalio.DigitalInOut(board.GP12)
+red_led.direction = digitalio.Direction.OUTPUT
+
+grn_led = digitalio.DigitalInOut(board.GP11)
+grn_led.direction = digitalio.Direction.OUTPUT
+
+blu_led = digitalio.DigitalInOut(board.GP10)
+blu_led.direction = digitalio.Direction.OUTPUT
+
+# setup media keys
 key_volume_up = digitalio.DigitalInOut(board.GP19)
 key_volume_up.switch_to_input(pull=digitalio.Pull.DOWN)
 
@@ -41,21 +54,33 @@ key_calc = digitalio.DigitalInOut(board.GP20)
 key_calc.switch_to_input(pull=digitalio.Pull.DOWN)
 
 
+# loop forever
 while True:
     if key_volume_up.value:
         keyboard.send(KEY_MEDIA_VOLUMEUP)
         led.value = True
+        red_led.value = True
+        blu_led.value = True
         time.sleep(0.1)
 
     if key_volume_down.value:
         keyboard.send(KEY_MEDIA_VOLUMEDOWN)
         led.value = True
+        grn_led.value = True
         time.sleep(0.1)
 
     if key_calc.value:
         keyboard.send(KEY_MEDIA_CALC)
         led.value = True
-        time.sleep(0.1)
+        blu_led.value = True
+        time.sleep(0.25)		# short pause to "debounce" calc key
 
     time.sleep(0.1)
+
+    # turn off leds
     led.value = False
+    red_led.value = False
+    grn_led.value = False
+    blu_led.value = False
+
+
